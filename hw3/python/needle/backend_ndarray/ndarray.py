d6810edd9afc315fa7f82e75e2a3bff86e1c517b
paoxiaode <1435709051@qq.com>
2023-04-20 11:15:51 +0000

    add array op

--- hw3/python/needle/backend_ndarray/ndarray.py
+++ hw3/python/needle/backend_ndarray/ndarray.py
@@ -293,7 +293,13 @@ class NDArray:
         """
 
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        _stride = list(self.strides)
+        for index, (i, j) in enumerate(zip(self.shape, new_shape)):
+            if i == 1:
+                _stride[index] = 0
+            elif i != 1 and i != j:
+                return ValueError()
+        return self.as_strided(new_shape,tuple(_stride))
         ### END YOUR SOLUTION
 
     ### Get and set elements
@@ -356,7 +362,18 @@ class NDArray:
         assert len(idxs) == self.ndim, "Need indexes equal to number of dimensions"
 
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        _shape = []
+        _strides = []
+        _offset = 0
+        for i, idx in enumerate(idxs):
+            start, stop, step = idx.start, idx.stop, idx.step
+            if stop > self.shape[i]: stop = self.shape[i]
+            _shape.append(math.ceil((stop - start) / step))
+            _offset += (start * self.strides[i])
+            _strides.append(self.strides[i] * step)
+        return NDArray.make(
+            tuple(_shape), strides=tuple(_strides), device=self.device, handle=self._handle, offset=_offset
+        )
         ### END YOUR SOLUTION
 
     def __setitem__(self, idxs, other):
